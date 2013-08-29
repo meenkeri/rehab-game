52c64de7f71635af1391953ff878cb1cbed2694a
unknown <shankara.meenkeri@WVMSANSMEENKERI.corp.service-now.com>
2013-08-28 19:23:30 -0700

    removing ModelManager game component to make the code easy

--- rehabGame/rehabGame/Game1.cs
+++ rehabGame/rehabGame/Game1.cs
@@ -8,6 +8,7 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using WiimoteLib;
 
 namespace rehabGame
 {
@@ -16,13 +17,43 @@ namespace rehabGame
     /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
+        public Matrix boardRotation = Matrix.Identity;
+        public Matrix board1Movement = Matrix.Identity;
+        public Matrix board2Movement = Matrix.Identity;
+        public Matrix board3Movement = Matrix.Identity;
+        public Matrix ballRotation = Matrix.Identity;
+        public Matrix boardMovement = Matrix.Identity;
+
+        Vector3 pos = new Vector3(0, 150, 100);
+        Vector3 target = Vector3.Zero;
+        Vector3 cameraPosition;
+        Vector3 cameraDirection;
+
+        float yawAngle = 0; //Left Right
+        float pitchAngle = 0; // Up Down
+        float rollAngle = 0; //Do nothing
+
+        public Matrix view { get; protected set; }
+        public Matrix projection { get; protected set; }
+
+        Vector3 ballPosition = new Vector3(0, -4.1F, 0);
+        Vector3 board1Position = new Vector3(0, 0, -200);
+        Vector3 board2Position = new Vector3(0, 0, -400);
+        Vector3 board3Position = new Vector3(0, 0, -800);
+
+        public Matrix boardWorld = Matrix.Identity;
+        public Matrix ballWorld = Matrix.Identity;
+        Model[] balls = new Model[1];
+        Model[] boards = new Model[5];
+
+        public Wiimote bb = new Wiimote();
+
         public static Boolean blank = false;
 
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         public Camera camera { get; protected set; }
-        ModelManager modelManager;
-
+        
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -43,8 +74,24 @@ namespace rehabGame
         {
             //camera = new Camera(this, new Vector3(0, 150, 120), Vector3.Zero, Vector3.Down);
             //Components.Add(camera);
-            modelManager = new ModelManager(this);
-            Components.Add(modelManager);
+            cameraPosition = pos;
+            cameraDirection = target - pos;
+            cameraDirection.Normalize();
+            CreateLookAt();
+
+            // Initialize projection matrix
+            projection = Matrix.CreatePerspectiveFieldOfView(
+                MathHelper.PiOver4,
+                (float)Window.ClientBounds.Width /
+                (float)Window.ClientBounds.Height,
+                1, 1000);
+
+            try
+            {
+                bb.Connect();
+                bb.SetLEDs(1);
+            }
+            catch { Console.WriteLine("Can't find a BalanceBoard"); }
             base.Initialize();
         }
 
@@ -56,6 +103,11 @@ namespace rehabGame
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            balls[0] = Content.Load<Model>(@"Models\ball");
+            boards[0] = Content.Load<Model>(@"Models\board");
+            boards[1] = Content.Load<Model>(@"Models\board");
+            boards[2] = Content.Load<Model>(@"Models\board");
+            boards[3] = Content.Load<Model>(@"Models\board");
 
             // TODO: use this.Content to load your game content here
         }
@@ -79,10 +131,13 @@ namespace rehabGame
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                this.Exit();
-            
+            boardUpdate();
+            board1Update();
+            board2Update();
+            board3Update();
+            ballUpdate();
+            ballUpdate1();
+
             base.Update(gameTime);
         }
 
@@ -93,8 +148,137 @@ namespace rehabGame
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Black);
+            BoardHelper.boardDraw(boardMovement, boards[0], boardRotation, projection, view);
+            BoardHelper.boardDraw(board1Movement, boards[1], boardRotation, projection, view);
+            BoardHelper.boardDraw(board2Movement, boards[2], boardRotation, projection, view);
+            BoardHelper.boardDraw(board3Movement, boards[3], boardRotation, projection, view);
+            BallHelper.ballDraw(balls[0], ballWorld, ballRotation, projection, view);
             if (!blank)
                 base.Draw(gameTime); 
+        }
+
+        public void boardUpdate()
+        {
+            WiimoteState s = bb.WiimoteState;
+            BalanceBoardState bbs = s.BalanceBoardState;
+            yawAngle -= bbs.CenterOfGravity.X * 0.0009F;
+            pitchAngle -= bbs.CenterOfGravity.Y * 0.0009F;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                yawAngle += 0.01F;
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                yawAngle -= 0.01F;
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+                pitchAngle += 0.01F;
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+                pitchAngle -= 0.01F;
+            if (yawAngle >= 0.05F)
+                yawAngle = 0.05F;
+            if (yawAngle <= -0.05F)
+                yawAngle = -0.05F;
+            if (pitchAngle >= 0.05F)
+                pitchAngle = 0.05F;
+            if (pitchAngle <= -0.05F)
+                pitchAngle = -0.05F;
+        }
+
+        public void board1Update()
+        {
+            boardRotation = Matrix.CreateRotationZ(MathHelper.Pi / 2);
+
+            //Move model
+            board1Movement = Matrix.CreateTranslation(board1Position);
+
+            //Rotate model
+            boardRotation *= Matrix.CreateFromYawPitchRoll(yawAngle, pitchAngle, rollAngle);
+        }
+
+        public void board2Update()
+        {
+            boardRotation = Matrix.CreateRotationZ(MathHelper.Pi / 2);
+
+            //Move model
+            board2Movement = Matrix.CreateTranslation(board2Position);
+
+            //Rotate model
+            boardRotation *= Matrix.CreateFromYawPitchRoll(yawAngle, pitchAngle, rollAngle);
+        }
+
+        public void board3Update()
+        {
+            boardRotation = Matrix.CreateRotationZ(MathHelper.Pi / 2);
+
+            //Move model
+            board3Movement = Matrix.CreateTranslation(board3Position);
+
+            //Rotate model
+            boardRotation *= Matrix.CreateFromYawPitchRoll(yawAngle, pitchAngle, rollAngle);
+        }
+        public void ballUpdate()
+        {
+            WiimoteState s = bb.WiimoteState;
+            BalanceBoardState bbs = s.BalanceBoardState;
+            ballPosition.X -= bbs.CenterOfGravity.X * 0.05F;
+            ballPosition.Z += bbs.CenterOfGravity.Y * 0.05F;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                ballPosition += Vector3.Left * 0.2F;
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                ballPosition += Vector3.Right * 0.2F;
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+                ballPosition += Vector3.Forward * 0.2F;
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+                ballPosition += Vector3.Backward * 0.2F;
+
+            if (ballPosition.X < -24)
+                ballPosition.X = -24;
+            if (ballPosition.X > 24)
+                ballPosition.X = 24;
+            if (ballPosition.Z < -18)
+                ballPosition.Z = -18;
+            if (ballPosition.Z > 17)
+                ballPosition.Z = 17;
+        }
+
+        public void ballUpdate1()
+        {
+            if (ballPosition.X > 12 && ballPosition.Y >= -4.1F && ballPosition.Y < 76 && ballPosition.Z < -12)
+                dropTheBall();
+
+            if (ballPosition.X < 12 && ballPosition.Y >= 76 && ballPosition.Y < 160 && ballPosition.Z < 12)
+                dropTheBall1();
+
+            //Move model
+            ballWorld = Matrix.CreateTranslation(ballPosition);
+        }
+
+        public void dropTheBall()
+        {
+            ballPosition += Vector3.Up * 0.6F;
+            if (ballPosition.Y >= 76)
+                ballPosition.Y = 76;
+            cameraPosition.Z -= 1.5F;
+            if (cameraPosition.Z <= -90)
+                cameraPosition.Z = -90;
+            CreateLookAt();
+
+        }
+
+        public void dropTheBall1()
+        {
+            ballPosition += Vector3.Up * 0.6F;
+            if (ballPosition.Y >= 153)
+                ballPosition.Y = 153;
+            cameraPosition.Z -= 1.5F;
+            if (cameraPosition.Z <= -280)
+                cameraPosition.Z = -280;
+            CreateLookAt();
+
+        }
+
+        private void CreateLookAt()
+        {
+            view = Matrix.CreateLookAt(cameraPosition, cameraPosition + cameraDirection, Vector3.Down);
         }
     }
 }
