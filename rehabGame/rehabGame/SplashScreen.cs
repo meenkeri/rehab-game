2f92b27d1729cf38d24288724707844543107780
unknown <shankara.meenkeri@WVMSANSMEENKERI.corp.service-now.com>
2015-04-05 13:09:26 -0700

    Adjust the greeting splash screen font size and the placement on the screen

--- rehabGame/rehabGame/SplashScreen.cs
+++ rehabGame/rehabGame/SplashScreen.cs
@@ -85,10 +85,10 @@ namespace rehabGame
             Vector2 TitleSize = spriteFont.MeasureString(textToDraw);
 
             //Draw main text
-            spriteBatch.DrawString(spriteFont, textToDraw, new Vector2(Game.Window.ClientBounds.Width / 2 - TitleSize.X / 2, Game.Window.ClientBounds.Height / 2), Color.White);
+            spriteBatch.DrawString(spriteFont, textToDraw, new Vector2(Game.Window.ClientBounds.Width / 2 - TitleSize.X / 2, Game.Window.ClientBounds.Height / 2 - 100), Color.White);
 
             //Draw subtext
-            spriteBatch.DrawString(secondarySpriteFont, secondaryTextToDraw, new Vector2(Game.Window.ClientBounds.Width / 2 - secondarySpriteFont.MeasureString(secondaryTextToDraw).X / 2, Game.Window.ClientBounds.Height / 2 + TitleSize.Y + 10), Color.White);
+            spriteBatch.DrawString(secondarySpriteFont, secondaryTextToDraw, new Vector2(Game.Window.ClientBounds.Width / 2 - secondarySpriteFont.MeasureString(secondaryTextToDraw).X / 2, Game.Window.ClientBounds.Height / 2 + TitleSize.Y + 10 - 100), Color.White);
 
             if (currentGameState == Game1.GameState.END)
             {
